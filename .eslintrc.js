54633075fee39b6d33813a7899d7b49f5b2fd936
phoebe <phoebe4561@gmail.com>
2021-10-21 10:59:30 +0800

    set prettier and test .env

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
+    'prettier/prettier': 'off',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
