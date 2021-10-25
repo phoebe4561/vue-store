2ab8b486bf82da1032c2644c72256dc58cd259f1
phoebe <phoebe4561@gmail.com>
2021-10-25 14:57:47 +0800

    revised loginbg

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import { createApp } from 'vue';
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 import App from './App.vue';
 import router from './router';
 
+// sweetalert
+const options = {
+  toast: true,
+  padding: '0.5rem',
+  //   position: 'top',
+  showConfirmButton: false,
+  //   confirmButtonColor: 'btn btn-primary',
+  //   cancelButtonColor: '#aaa',
+  //   cancelButtonText: '取消',
+  timer: 2000,
+};
+
 const app = createApp(App);
 app.use(VueAxios, axios);
+app.use(VueSweetalert2, options);
 app.use(router);
 app.mount('#app');
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,17 +5,25 @@
         <div class="col-md-6 loginBg"></div>
         <div class="col-md-6 justify-content-center align-items-center d-flex vh-100 me-auto ms-auto">
           <div class="loginForm">
-            <h1 class="text-center py-3">Login</h1>
-            <form>
+            <h1 class="text-center py-3">LOG IN</h1>
+            <form @submit.prevent="signIn">
               <div class="form-floating py-2">
-                <input class="form-control" type="email" id="inputEmail" placeholder="name@example.com" />
+                <input v-model="user.username" class="form-control" type="email" id="inputEmail" placeholder="name@example.com" />
                 <label class="py-4" for="inputEmail">Email Address</label>
               </div>
-              <div class="form-floating py-2">
-                <input class="form-control" type="password" id="inputPassword" placeholder="password" />
+              <div class="form-floating position-relative py-2">
+                <input v-model="user.password" class="form-control" :type="canSeePw ? 'text' : 'password'" id="inputPassword" placeholder="password" />
+                <button @click="canSeePw = !canSeePw" class="btn watchPw position-absolute" type="button">
+                  <i class="fas fa-eye-slash" v-if="!canSeePw"></i>
+                  <i class="fas fa-eye" v-else></i>
+                </button>
                 <label class="py-4" for="inputPassword">Password</label>
               </div>
-              <button type="submit" class="btn btn-primary w-100 py-2 mt-3">登入</button>
+              <button type="submit" class="btn btn-primary w-100 py-2 mt-3">
+                <i class="fas fa-spinner fa-pulse me-2" v-if="loadingState === 'loging'"></i>
+                <template v-if="loadingState === 'loging'">登入中</template>
+                <template v-else>登入</template>
+              </button>
             </form>
           </div>
         </div>
@@ -24,17 +32,75 @@
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+      loadingState: '',
+      canSeePw: false,
+    };
+  },
+  methods: {
+    signIn() {
+      if (!this.user.username || !this.user.password) {
+        this.$swal.fire({ icon: 'warning', title: '請勿空白' });
+        return;
+      }
+      this.loadingState = 'loging';
+      const api = `${process.env.VUE_APP_API}admin/signin`;
+      this.$http
+        .post(api, this.user)
+        .then((res) => {
+          const { success, message, token, expired } = res.data;
+          if (success) {
+            document.cookie = `token=${token}; expires=${new Date(expired)}; path=/`;
+            const name = this.user.username.split('@')[0];
+            // console.log(name);
+            sessionStorage.setItem('userId', name);
+            this.$router.push('/home');
+          } else {
+            this.user.password = '';
+            this.loadingState = '';
+            this.$swal.fire({ icon: 'error', title: message });
+          }
+        })
+        .catch((err) => {
+          this.$swal.fire({ icon: 'error', title: err.message });
+        });
+    },
+  },
+};
+</script>
+
 <style lang="scss">
+@import '@/assets/style/all';
 .loginBg {
-  background: url('../assets/images/temp.jpg') center 80% / cover no-repeat;
+  background: url('../assets/images/loginbg.jpeg') center 80% / cover no-repeat;
   height: 100vh;
   position: absolute;
   z-index: -1;
+  background-position: 50% 100%;
 }
 .loginForm {
   max-width: 500px;
   width: 100%;
   padding: 1rem;
   background-color: rgba(0, 100, 0, 0.2);
+}
+
+.watchPw {
+  right: 5px;
+  top: 20px;
+  i {
+    font-size: 20px;
+  }
+}
+
+body.swal2-toast-shown .swal2-container.swal2-center {
+  top: 90%;
 }
 </style>
